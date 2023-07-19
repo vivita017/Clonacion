90326290b1a25f56291f5513d58c167555663e8a
vivita017 <sofvivi.jasso.osorio@gmail.com>
2023-07-19 17:49:24 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById('app-dropdown').style.cssText = 'display:block';
+}
+
+function removemyFunction() {
+    document.getElementById('app-dropdown').style.cssText = 'display:none';
+}
